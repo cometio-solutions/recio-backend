888191a287059232d30d16208750fc43f8d163d0
jmysliv <jjmreal34@gmail.com>
2021-03-18 22:56:22 +0100

    Adjusted to review

--- rest/app.py
+++ rest/app.py
@@ -1,17 +1,17 @@
+import os
 from flask import Flask
 import pymysql
-import os
 
 app = Flask(__name__)
 
 conf = {
     "host": os.environ['HOST'],
-    "port": 3306,
+    "port": int(os.environ['PORT']),
     "user": os.environ['USER'],
     "passwd": os.environ['PASSWORD'],
     "charset": "utf8mb4",
     "cursorclass": pymysql.cursors.DictCursor,
-    "database": "recio"
+    "database": os.environ['DATABASE']
 }
 
 conn = pymysql.connect(**conf)
@@ -19,6 +19,7 @@ cursor = conn.cursor()
 cursor.execute("SHOW TABLES")
 
 print(cursor.fetchall())
+
 
 @app.route('/')
 def hello():
