ae1a7d14b417fb5a0262bd8784a4aaf524c591a4
twistzzmc <michal.szczepaniak1999@gmail.com>
2021-03-26 15:10:24 +0100

    chenged user route dir, fixed register, added few tests

--- rest/__main__.py
+++ rest/__main__.py
@@ -1,10 +1,5 @@
 import json
-import re
 from rest.app import create_app, setup_database
-from rest.models.user import User
-from rest.models.editor_request import EditorRequest
-from rest.db import db
-from flask import request
 
 
 app = create_app()
@@ -21,102 +16,6 @@ def hello():
     )
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
-
-@app.route('/user', methods=['POST'])
-def register():
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-    editor_request = request.form.get('editorRequest')
-
-    data = {}
-    if len(name) < 3 or len(name) > 30 or re.match('^[.a-zA-Z0-9_-]+$', name) is None:
-        data["invalid_name"] = name
-    if not email.endswith('agh.edu.pl') or re.match('^[.@a-zA-Z0-9_-]+$', email) is None:
-        data["invalid_email"] = email
-    if len(password) < 3 or len(password) > 30 or re.match('^[.a-zA-Z0-9_-]+$', password) is None:
-        data["invalid_password"] = password
-
-    if len(data) > 0:
-        response = app.response_class(
-            response=json.dumps(data),
-            status=400,
-            mimetype='application/json'
-        )
-        return response
-
-    check_user = User.query.filter_by(email=email).first()
-
-    if check_user:
-        response = app.response_class(
-            response=json.dumps({"email_taken": email}),
-            status=409,
-            mimetype='application/json'
-        )
-        return response
-
-    new_user = User(email, name, password, False)
-    db.session.add(new_user)
-
-    if editor_request:
-        new_editor_request = EditorRequest(email, name)
-        db.session.add(new_editor_request)
-
-    db.session.commit()
-
-    response = app.response_class(
-        response=json.dumps({"name": name, "email": email, "password": password, "editorRequest": editor_request}),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-
-@app.route('/user/auth', methods=['POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()
-
-    if user and user.password == password:
-        data = {'auth': True}
-        status = 200
-    else:
-        data = {'auth': False}
-        status = 400
-
-    response = app.response_class(
-            response=json.dumps(data),
-            status=status,
-            mimetype='application/json'
-        )
-    if status == 200:
-        response.set_cookie('email', value=email)
-
-    return response
-
-
-@app.route('/user/editorRequests', methods=['GET'])
-def admin_editor_requests():
-    if 'email' not in request.cookies:
-        return app.response_class(response=json.dumps({'cookie': False}), status=401, mimetype='application/json')
-
-    email = request.cookies.get('email')
-    user = User.query.filter_by(email=email).first()
-
-    if not user or not user.is_editor:
-        return app.response_class(response=json.dumps({'is_editor': False}), status=403, mimetype='application/json')
-
-    users = User.query.all()
-    data = [{'name': u.name, 'user_email': u.email} for u in users]
-
-    return app.response_class(
-            response=json.dumps(data),
-            status=200,
-            mimetype='application/json'
-        )
 
 
 app.run(host='0.0.0.0')
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,44 +1,76 @@
+import json
 import requests
 
 
-url_user = "http://127.0.0.1:5000/user"
-url_user_auth = "http://127.0.0.1:5000/user/auth"
-url_user_editor = "http://127.0.0.1:5000/user/editorRequests"
+URL_USER = 'http://127.0.0.1:5000/user'
+URL_USER_AUTH = 'http://127.0.0.1:5000/user/auth'
+URL_USER_EDITOR = 'http://127.0.0.1:5000/user/editorRequests'
 
 
 def test_valid_registration_input():
-    data = {"name": "proper_name", "email": "proper@agh.edu.pl", "password": "12345", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 200
+    data = {'name': 'proper_name', 'email': 'proper@agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 200
 
-    data = {"name": "proper_name", "email": "proper@agh.edu.pl", "password": "12345", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 409
+    data = {'name': 'proper_name', 'email': 'student@student.agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 200
 
-    data = {"name": "1", "email": "proper@agh.edu.pl", "password": "12345", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 400
+    data = {'name': 'proper_name', 'email': 'proper@agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 409
 
-    data = {"name": "123", "email": "proper@gh.edu.pl", "password": "12345", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 400
+    data = {'name': '1', 'email': 'proper@agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 400
 
-    data = {"name": "123", "email": "proper@agh.edu.pl", "password": "45", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 400
+    data = {'name': '123', 'email': 'proper@gh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 400
 
-    data = {"name": "proper_name ", "email": "proper@agh.edu.pl", "password": "12345", "editorRequest": False}
-    assert requests.post(url_user, data=data).status_code == 400
+    data = {'name': '123', 'email': 'proper@agh.edu.pl',
+            'password': '45', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 400
+
+    data = {'name': 'proper_name ', 'email': 'proper@agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    assert requests.post(URL_USER, data=data).status_code == 400
 
 
 def test_login():
-    data = {"name": "proper_name", "email": "login@agh.edu.pl", "password": "12345", "editorRequest": False}
-    requests.post(url_user, data=data)
-    login_data = {"email": "login@agh.edu.pl", "password": "12345"}
-    assert requests.post(url_user_auth, data=login_data).status_code == 200
+    data = {'name': 'proper_name', 'email': 'login@agh.edu.pl',
+            'password': '12345', 'editorRequest': False}
+    requests.post(URL_USER, data=data)
+    login_data = {'email': 'login@agh.edu.pl', 'password': '12345'}
+    assert requests.post(URL_USER_AUTH, data=login_data).status_code == 200
 
-    login_data = {"email": "login2@agh.edu.pl", "password": "12345"}
-    assert requests.post(url_user_auth, data=login_data).status_code == 400
+    login_data = {'email': 'login2@agh.edu.pl', 'password': '12345'}
+    assert requests.post(URL_USER_AUTH, data=login_data).status_code == 400
 
 
 def test_admin_editor_request():
-    data = {"name": "proper_name", "email": "register@agh.edu.pl", "password": "12345", "editorRequest": False}
-    requests.post(url_user, data=data)
-    assert requests.get(url_user_editor).status_code == 401
-    assert requests.get(url_user_editor, cookies={"email": "register@agh.edu.pl"}).status_code == 403
-    assert requests.get(url_user_editor, cookies={"email": "non_existed_register@agh.edu.pl"}).status_code == 403
+    data = {'name': 'proper_name', 'email': 'register@agh.edu.pl',
+            'password': '12345', 'editorRequest': True}
+    requests.post(URL_USER, data=data)
+
+    assert requests.get(URL_USER_EDITOR).status_code == 401
+
+    assert requests.get(
+        URL_USER_EDITOR,
+        cookies={'email': 'register@agh.edu.pl'}
+    ).status_code == 403
+
+    assert requests.get(
+        URL_USER_EDITOR,
+        cookies={'email': 'non_existed_register@agh.edu.pl'}
+    ).status_code == 403
+
+    r = requests.get(
+        URL_USER_EDITOR,
+        cookies={'email': 'admin@admin.agh.edu.pl'}
+    )
+    assert r.status_code == 200
+    data = json.loads(r.content.decode('utf-8'))
+    assert len(data) == 1
+    assert data[0]['name'] == 'proper_name'
+    assert data[0]['user_email'] == 'register@agh.edu.pl'
