83f0e01fb9cdaa77e313336cb88a6c05210bd462
twrdyyy <filip.twardy.v01@gmail.com>
2021-04-08 20:19:27 +0200

    [CS-22] Fix tests, email already taken by different test

--- tests/test_recruitment.py
+++ tests/test_recruitment.py
@@ -12,12 +12,12 @@ def test_recruitment_data():
     url_user_auth = 'http://127.0.0.1:5000/user/auth'
     url_recruitment = 'http://127.0.0.1:5000/recruitment'
 
-    data = {'name': 'proper_name', 'email': 'register_req@test.agh.edu.pl',
+    data = {'name': 'recruitment_test', 'email': 'recruitment@test.agh.edu.pl',
             'password': '12345', 'editorRequest': True}
 
     assert requests.post(url_user, json=data).status_code == 200
 
-    data = {'email': 'register_req@test.agh.edu.pl', 'password': '12345'}
+    data = {'email': 'recruitment@test.agh.edu.pl', 'password': '12345'}
     response = requests.post(url_user_auth, json=data)
     non_admin_login = json.loads(response.content.decode('utf-8'))
 
