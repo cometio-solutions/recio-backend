04d5e9c02cd2d0c340e2080b05304783985a56c2
Filip Twardy <34160783+twrdyyy@users.noreply.github.com>
2021-04-15 00:33:42 +0200

    [fix/CS-66] Fix nginx (#26)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+echo "Updating pip..."
+pip3 install --upgrade pip
+echo "Pip updated."
 echo "Installing requirements..."
 pip3 install -r requirements.txt > /dev/null
 echo "Requirements installed."
