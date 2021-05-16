df135849c4fd468008158ef450d8bf6d51b0b163
twrdyyy <filip.twardy.v01@gmail.com>
2021-05-16 17:27:26 +0200

    [CS-69] Add migration endpoint

--- rest/routes/candidate.py
+++ rest/routes/candidate.py
@@ -7,16 +7,21 @@ from flask import Blueprint, request
 from rest.common.response import create_response
 from rest.common.token import handle_request_token
 from rest.models.candidate import Candidate
+from rest.models.candidate_recruitment import CandidateRecruitment
+from rest.models.recruitment import Recruitment
 
 candidate_url = Blueprint('candidate', __name__)
 
 
-@candidate_url.route('/<candidate_pesel>', methods=['GET'])
+@candidate_url.route('/<candidate_pesel>', methods=['GET', 'OPTIONS'])
 def get_candidate(candidate_pesel):
     """
     Endpoint returns all available information about specific candidate
     :returns: flask response containing json with candidate data
     """
+    if request.method == 'OPTIONS':
+        return create_response({}, 200, '*', 'content-type, token')
+
     logging.info("Getting candidate data")
 
     role, response = handle_request_token(request)
@@ -32,5 +37,38 @@ def get_candidate(candidate_pesel):
         return create_response({"error": "Nie ma urzytkownika o podanym peselu!"}, 404, '*')
 
     data['data'] = Candidate.to_json(candidate)
+
+    return create_response(data, 200, '*')
+
+
+@candidate_url.route('/migration/<candidate_pesel>', methods=['GET', 'OPTIONS'])
+def get_candidate_migration(candidate_pesel):
+    """
+    Endpoint returns candidate's recruitment history
+    :return: flask response containing json with recruitment history
+    """
+    if request.method == 'OPTIONS':
+        return create_response({}, 200, '*', 'content-type, token')
+
+    logging.info("Getting recruitment data")
+
+    role, response = handle_request_token(request)
+
+    if role is None:
+        logging.warning("Role is not specified!")
+        return response
+
+    data = dict()
+    candidate_recruitments = CandidateRecruitment.query\
+        .filter_by(candidate_pesel=candidate_pesel).all()
+    if len(candidate_recruitments) == 0:
+        logging.warning("No such user with given pesel!")
+        return create_response({"error": "Nie ma urzytkownika o podanym peselu!"}, 404, '*')
+
+    recruitments = [Recruitment.query.filter_by(id=can_rec.recruitment_id).first()
+                    for can_rec in candidate_recruitments]
+
+    data['data'] = [Recruitment.to_json(rec, len(rec.candidate_recruitments))
+                    for rec in recruitments]
 
     return create_response(data, 200, '*')
