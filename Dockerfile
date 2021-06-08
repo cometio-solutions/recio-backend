b7733d4947b033bbeeff72e0e110c5b8951bd60f
Filip Twardy <34160783+twrdyyy@users.noreply.github.com>
2021-06-08 22:38:49 +0200

    [Task CS-52] Introduce recruitment chart pdf generator (#45)
    * [CS-52] Introduce recruitment chart pdf generator
    
    * [CS-52] Add bar labels and values
    
    * [CS-52] Fix missing packages in requirements
    
    * [CS-52] Update alpine image
    
    * [CS-52] Finally fix matplotlib on docker image
    
    * [CS-52] Fix linter xd

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
-FROM alpine:3.8
+# FROM alpine:latest
+FROM openjdk:8-jre-alpine
+
+RUN apk add --no-cache tesseract-ocr python3 py3-numpy && \
+    pip3 install --upgrade pip setuptools wheel && \
+    apk add --no-cache --virtual .build-deps gcc g++ zlib-dev make python3-dev py-numpy-dev jpeg-dev && \
+    pip3 install matplotlib && \
+    apk del .build-deps
 RUN apk add python3
+RUN apk add cmd:pip3
 RUN apk add py-cryptography
 RUN apk add bash
 RUN apk add py3-sqlalchemy
---
+++ rest/generators/recruitment.py
@@ -0,0 +1,135 @@
+"""
+This script generates plots from recruitment data and saves them as pdf file
+"""
+import logging
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+def column_chart(
+        labels: list,
+        chart_name: str,
+        y_label: str,
+        **kwargs):
+    """
+    Return matplotlib figure with given args
+    Parameters
+    ----------
+    labels : list
+    chart_name : str
+    y_label : str
+
+    Returns
+    -------
+    chart : matplotlib figure
+    """
+
+    fig, axes = plt.subplots()
+
+    labels_num = len(labels)
+
+    x_pos = list(range(labels_num))
+    bar_width = 1.0 / labels_num - 0.1
+
+    idx = 0
+
+    max_val = 0
+
+    bars = []
+
+    for label, value in kwargs.items():
+        bars.append(axes.bar(
+                x_pos + idx * bar_width,
+                value,
+                bar_width,
+                label=label
+            )
+        )
+        idx += 1
+        max_val = max(max_val, max(value))
+
+    axes.set_ylabel(y_label)
+    axes.set_title(chart_name)
+    axes.set_xticks(x_pos + bar_width * (len(kwargs) - 1) / 2)
+    axes.set_ylim([0, int(1.3 * max_val)])
+    axes.set_xticklabels(labels, rotation=45)
+    axes.legend()
+
+    for bar_text in bars:
+        axes.bar_label(bar_text, padding=3)
+
+    fig.tight_layout()
+
+    return fig
+
+
+def generate_plots(recruitment_data: dict):
+    """
+    Generate plots for given recruitment data
+    You have to provide given fields:
+    {
+        "major_name"
+        "point_limit"
+        "slot_limit"
+        "candidates_num"
+    }
+    Parameters
+    ----------
+    recruitment_data : dict
+
+    Returns
+    -------
+    path to pdf file : str
+    """
+
+    keys = ["major_name", "point_limit", "slot_limit", "candidates_num"]
+    labels = []
+    slot_limits = []
+    point_limits = []
+    candidates_num = []
+
+    for rec in recruitment_data:
+        if not all(key in rec for key in keys):
+            logging.warning("Missing required fields!")
+            return ""
+
+        labels.append(rec['major_name'])
+        slot_limits.append(rec['slot_limit'])
+        point_limits.append(rec['point_limit'])
+        candidates_num.append(rec['candidates_num'])
+
+    with PdfPages("recruitment.pdf") as pdf:
+        column_chart(labels=labels,
+                     chart_name="Limit punktów do ilości kandydatów",
+                     y_label="",
+                     punkty=point_limits,
+                     kandydaci=candidates_num)
+        pdf.savefig()
+        plt.close()
+        column_chart(labels=labels,
+                     chart_name="limit punktów do limitu miejsc",
+                     y_label="",
+                     punkty=point_limits,
+                     miejsca=slot_limits)
+        pdf.savefig()
+        plt.close()
+        column_chart(labels=labels,
+                     chart_name="Limity punktów",
+                     y_label="",
+                     punkty=point_limits)
+        pdf.savefig()
+        plt.close()
+        column_chart(labels=labels,
+                     chart_name="Liczba miejsc",
+                     y_label="",
+                     miejsca=slot_limits)
+        pdf.savefig()
+        plt.close()
+        column_chart(labels=labels,
+                     chart_name="Liczba kandydatów",
+                     y_label="",
+                     kandydaci=candidates_num)
+        pdf.savefig()
+        plt.close()
+
+    return "recruitment.pdf"
